77be73d1f11cfb4634ae4b89194ba66729d911d1
ShowGo_JP <shogosakai73@gmail.com>
2019-08-17 17:15:57 +0900

    refactored user controller codes.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,17 @@
 class UsersController < ApplicationController
   before_action :set_birthdate, only: [:signup, :create, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update]
+
   def index
     @users = User.all
   end
 
   def signup
     @user = User.new
+  end
+
+  def login_form
+
   end
 
   def create
@@ -19,15 +25,12 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Edit Success!!"
       redirect_to user_path(params[:id])
@@ -36,38 +39,38 @@ class UsersController < ApplicationController
     end
   end
 
-  def destroy
-  end
-
   private
-  def user_params
-    # debugger
-    params.require(:user).permit(:nickname, :email, :password, :profile, :birthyear, :birthmonth, :birthday, :age, :gender, :image)
-  end
-
-    # 誕生日の表示メソッド
-  def set_birthdate
-    y = 1950
-    @year = []
-    while y <= 2009 do
-      @year << [y,y]
-      y += 1
+    def user_params
+      # debugger
+      params.require(:user).permit(:nickname, :email, :password, :profile, :birthyear, :birthmonth, :birthday, :age, :gender, :image)
     end
 
-    m = 1
-    @month = []
-    while m <= 12 do
-      @month << [m,m]
-      m += 1
+    def set_user
+      @user = User.find(params[:id])
     end
+      # 誕生日の表示メソッド
+    def set_birthdate
+      y = 1950
+      @year = []
+      while y <= 2009 do
+        @year << [y,y]
+        y += 1
+      end
 
-    d = 1
-    @day =[]
-    while d <=31 do
-      @day << [d,d]
-      d += 1
+      m = 1
+      @month = []
+      while m <= 12 do
+        @month << [m,m]
+        m += 1
+      end
+
+      d = 1
+      @day =[]
+      while d <=31 do
+        @day << [d,d]
+        d += 1
+      end
+
     end
-
-  end
 
 end
