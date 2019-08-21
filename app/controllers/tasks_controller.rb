456e2283c490939e1c3992582863089a55777759
ShowGo_JP <shogosakai73@gmail.com>
2019-08-21 17:47:45 +0900

    made task views, functions.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,10 @@
 class TasksController < ApplicationController
   before_action :set_current_items
-  # before_action :set_staff_name, only: [:new, :create]
+  before_action :set_tasks, only: :toggle
+  before_action :set_task, only: [:toggle, :edit, :update, :destroy]
+
   def index
-    @tasks = Task.where(live_id: @live.id)
+    @tasks = Task.where(live_id: @live.id).ord(finish_date: "ASC")
   end
 
   def new
@@ -22,15 +24,32 @@ class TasksController < ApplicationController
   end
 
   def edit
+    
   end
 
   def update
+    if @task.update(task_params)
+      @task.save
+      flash[:notice] = "Edit Success!"
+      redirect_to "/users/#{@current_user.id}/lives/#{@live.id}/#tasks"
+    else
+      @error_message = "Has some error. Please check again."
+      render :edit
+    end
   end
 
-  def delete
+  def destroy
+    @task.delete
+    redirect_to "/users/#{@current_user.id}/lives/#{@live.id}/#tasks"
+    flash[:notice] = "Task Deleted"
   end
 
   def show
+  end
+
+  def toggle
+    # debugger
+    @task.update_attributes(finish: !@task.finish)
   end
 
   private
@@ -38,6 +57,14 @@ class TasksController < ApplicationController
     # debugger
     params.require(:task).permit(:kind, :content, :start_date, :finish_date, :finish, :memo, :user_id ).merge(live_id: @live.id )
     # debugger
+  end
+
+  def set_tasks
+    @tasks = Task.where(live_id: @live.id)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 
   def set_current_items
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   end
 
   # タスク
+  post "toggle_finish/:id", to: "tasks#toggle"
   resources :tasks
   # ルート
   root "home#index"
