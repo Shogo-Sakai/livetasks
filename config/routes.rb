4ad47b23d7b015564fd6d19832b15e6691f91c4e
ShowGo_JP <shogosakai73@gmail.com>
2019-08-23 17:50:29 +0900

    fixed routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   get 'calendar/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # ユーザーログイン関係
-  get "/users/signup", to: "users#signup", as: "new_registration_user"
-  post "/users/signup", to:"users#create"
+  get "/signup", to: "users#signup", as: "new_registration_user"
+  post "/signup", to:"users#create"
   get "login", to:"users#login_form", as: "login_user"
   post "login", to:"users#login"
   post "logout", to:"users#logout"
