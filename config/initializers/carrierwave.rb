35db09d65d3cf58413e9bfdd65596ed339844279
shogosakai <shogosakai73@gmail.com>
2019-09-05 19:43:00 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,20 +4,18 @@ require 'carrierwave/storage/fog'
 
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.fog_provider = 'AWS'
+    config.fog_provider = 'fog/aws' 
     config.fog_credentials = {
       # Amazon S3用の設定
-      provider: 'AWS',
-      region: ENV['AWS_S3_REGION'],
-      aws_access_key_id: ENV['AWS_IAM_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_IAM_ACCESS_KEY'],
+      :provider              => 'AWS',
+      :region                => ENV['S3_REGION'], 
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['AWS_S3_BUCKET']
-    # config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
-    config.asset_host = "https://s3.ap-northeast-1.amazonaws.com/livetask"
-    config.cache_storage = :fog
+    config.fog_directory     =  ENV['S3_BUCKET']
+    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } 
   end
 
   # 日本語ファイル名の設定
-  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
+  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/ 
 end
