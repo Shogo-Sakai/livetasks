99d8ca3c59ccf64b2cd79fde17b0932cdc79486f
shogosakai <shogosakai73@gmail.com>
2019-10-17 10:51:29 +0900

    make task controller test contexts.

---
+++ spec/controllers/tasks_controller_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper" 
+
+describe TasksController do
+  context "GET #index"
+  context "GET #new"
+  context "POST #create"
+  context "GET #edit"
+  context "POST #upate"
+  context "DELETE #destroy"
+  context "GET show"
+end
